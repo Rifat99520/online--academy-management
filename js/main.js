dc408da826ea6e8e3b73b4e874323210aaf5bb9e
Rifat Anvar <rifata.work@gmail.com>
2023-10-28 12:44:30 +0530

    Initial code commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+require.config({
+    baseUrl: 'js',
+    paths : {
+        Student : 'student',
+        BaseController : 'BaseController',
+        
+    }
+});
+
+require(['BaseController'], function (BaseController) {
+    
+});
